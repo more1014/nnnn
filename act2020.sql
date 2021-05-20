19bac6b4034744ab985f2e5fe1c577d55d8a2116
more1014 <migamore@misena.edu.co>
2021-05-20 17:55:57 -0500

    secont part

--- act2020.sql
+++ act2020.sql
@@ -4,7 +4,7 @@ name varchar (50)primary key
 create table system_user_(
 id int primary key not null, 
 login varchar (50)not null,
-password varchar (60),
+password varchar (60) not null,
 email varchar (254)not null,
 activated int ,
 lang_key varchar(6)not null,
@@ -92,5 +92,79 @@ id int not null primary key,
 	
 ) 
 
+create table aprendiz(
+    id int not null primary key,
+	id_cliente int not null,
+	id_ficha int not null ,
+	id_estado_formacion int not null,
+	constraint uk_aprendiz unique (id_cliente,id_ficha),
+    constraint fk_esta_apre foreign key (id_cliente)references cliente (id),
+	constraint fk_fich_apre foreign key (id_ficha)references ficha(id),
+	constraint fk_esta_apre_ foreign key (id_estado_formacion)references estado_formacion (id)
+)
+create table ficha (
+id int primary key not null,
+id_programa int not null,
+numero_ficha varchar (100)not null, 
+fecha_inicio date not null,
+fecha_fin date not null,
+ruta varchar (40)not null,
+id_estado_ficha int not null,
+id_jornada int not null,
+constraint uk_ficha unique(numero_ficha),
+constraint fk_prog_fich foreign key (id_programa)references programa (id),
+constraint fk_esfi_fich foreign key (id_estado_ficha)references estado_ficha (id),
+constraint fk_jorn_fich foreign key (id_jornada)references jornada (id)
+)
+create table estado_ficha(
+id int not null primary key,
+nombre_estado varchar (20)not null,
+estado int not null,
+constraint uk_nombre_estado_ unique (nombre_estado)
+)
+create table jornada (
+id int not null primary key,
+	sigla_jornada varchar (20)not null,
+	nombre_jornada varchar(40)not null,
+	describcion varchar (100)not null,
+	imagen_url varchar (1000),
+	estado varchar (40)not null,
+	constraint uk_sigla_jornada unique (sigla_jornada),
+	constraint uk_nombre_jornada unique (nombre_jornada)
+)
 
-
+create table trimestre (
+id int not null primary key ,
+nombre_trimestre int not null,
+id_jornada int not null,
+id_nivel_formacion int not null,
+estado varchar (40)not null,
+constraint fk_jorn_trim foreign key (id_jornada)references jornada (id),
+constraint fk_nifo_trim foreign key (id_nivel_formacion)references nivel_formacion(id),
+constraint uk_trimestre_ unique (nombre_trimestre,id_jornada,id_nivel_formacion)
+)
+create table ficha_has_trimetre(
+id int not null primary key ,
+id_ficha int not null,
+id_trimestre int not null,
+constraint uk_ficha_has_trimestre unique (id_ficha,id_trimestre),	
+constraint fk_trim_fitr foreign key (id_trimestre)references trimestre(id),
+constraint	fk_fht foreign key (id_ficha)references ficha (id)
+)
+create table planeacion(
+	id int not null primary key,
+	codigo varchar (40)not null, 
+	fecha date not null,
+	estado varchar (40)not null,
+	constraint uk_codigo unique (codigo)
+	
+) 
+create table ficha_planeacion(
+id int not null primary key,
+	id_ficha int not null,
+	id_planeacion int not null,
+	estado varchar (40) not null,
+	constraint uk_ficha_planeacion unique (id_ficha,id_planeacion),
+    constraint fk_fich_apre foreign key (id_ficha)references ficha (id),
+	constraint fk_fich_pln foreign key  (id_planeacion)references planeacion(id)
+)
